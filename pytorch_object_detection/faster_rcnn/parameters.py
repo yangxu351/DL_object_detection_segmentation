22f0a94e34c9b1032fd9a2a28b0ebf09ce73d912
yang.xu <1554404832@qq.com>
2021-10-15 08:11:40 -0400

    add BASE_DIR, DATA_SEED

--- pytorch_object_detection/faster_rcnn/parameters.py
+++ pytorch_object_detection/faster_rcnn/parameters.py
@@ -1,8 +1,11 @@
+BASE_DIR= '/media/lab/Yang'  # Thor
+BASE_DIR= '/data/users/yang' # Groot
 
 CMT = 'syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40'
+DATA_SEED = 0
 
 train_syn = True
-EPOCHS = 2 # 20
+EPOCHS = 20 # 20
 BATCH_SIZE = 8
 LEARNING_RATE = 0.05 
 ### with pixel attention branch, 
@@ -10,7 +13,7 @@ WITH_PA = False # True False,  Pixel attention
 ### with FPN Mask multiply
 WITH_FPN_MASK = False # True False,  
 ### with RPN Mask multiply
-WITH_RPN_MASK = True # True False
+WITH_RPN_MASK = False # True False
  
 # SOFT_VAL = 1 
 if WITH_RPN_MASK:
