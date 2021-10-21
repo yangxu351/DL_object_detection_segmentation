5ebd57662d409cc8659981d7ba653e2f165819c3
yangxu351 <xuyang_ustb@163.com>
2021-10-21 08:08:32 -0400

    plot soft_value masks

--- pytorch_object_detection/faster_rcnn/object_score_util/plot_mask.py
+++ pytorch_object_detection/faster_rcnn/object_score_util/plot_mask.py
@@ -19,21 +19,26 @@ if __name__ == '__main__':
     white_target_dir = syn_args.syn_data_segs_dir + '_white_target'
     if not os.path.exists(white_target_dir):
         os.mkdir(white_target_dir)
-    seg_files = glob.glob(os.path.join(syn_args.syn_data_segs_dir, f'*{IMG_FORMAT}'))
+    seg_files = glob.glob(os.path.join(syn_args.syn_data_segs_dir, f'*{IMG_FORMAT}'))[:20]
+    
+    # file_names = ['syn_wdt_BlueSky_sd1668_ca0_ch58_86'+IMG_FORMAT, 'syn_wdt_CloudySky_sd3654_ca0_ch53_195'+IMG_FORMAT]
+    # seg_files = [os.path.join(syn_args.syn_data_segs_dir, name) for name in file_names]
+    
     np.random.seed(DATA_SEED)
-    for f in seg_files[:10]:
+    for f in seg_files:
         img = cv2.imread(f, cv2.COLOR_BGR2GRAY) # black are targets, white are backgrounds
         img = 255-img # white are targets, black are backgrounds
-        # cv2.imwrite(os.path.join(white_target_dir, os.path.basename(f)), img)
+        cv2.imwrite(os.path.join(white_target_dir, os.path.basename(f)), img)
         print('img shape', img.shape, 'non_zeros', np.count_nonzero(img!=0))
-        print('unique values', np.unique(img))
-        msk = np.random.random_sample(size=img.shape)*254
+        # print('unique values', np.unique(img))
+        msk = np.random.random_sample(size=img.shape)*254//2
         msk = np.round(msk).astype(np.int)
         ############## v1
-        # msk[img==255] = 255
-        # print(np.count_nonzero(msk[img==255]))
+        msk[img==255] = 255
+        print(np.count_nonzero(msk[img==255]))
         ############## v2
-        msk[img!=0] = 255
-        print(np.count_nonzero(msk[img!=0]))
+        # msk[img!=0] = 255
+        # print(np.count_nonzero(msk[img!=0]))
+        ##############
         cv2.imwrite(os.path.join(soft_msk_dir, os.path.basename(f)), msk)
         
