75f50d0f50043765bc53d15bf6689935d31643e4
yangxu351 <xuyang_ustb@163.com>
2022-03-15 23:41:03 -0400

    adjust rpn soft value combine

--- pytorch_object_detection/faster_rcnn/network_files/rpn_function.py
+++ pytorch_object_detection/faster_rcnn/network_files/rpn_function.py
@@ -281,21 +281,18 @@ class RPNMaskHead(nn.Module):
                 msk = F.interpolate(masks, size=feat_shape, mode="nearest")
                 if self.soft_val == -1:
                     soft_msk = torch.rand_like(msk)
-                    ## fixme
-                    soft_msk[msk==1] = 1
-                    ### fixme softval-1_nonzero
-                    #  soft_msk[msk!=0] = 1
-                    msk=soft_msk
+
                 elif self.soft_val == -0.5:
                     ### fixme softval-1_halfmax
                     soft_msk = torch.rand_like(msk)//2
-                    ## fixme
-                    soft_msk[msk==1] = 1
-                    ### fixme softval-1_nonzero
-                    #  soft_msk[msk!=0] = 1
-                    msk=soft_msk
                 else:
-                    msk[msk==0] = self.soft_val
+                    # msk[msk==0] = self.soft_val
+                    soft_msk = torch.ones_like(msk)*self.soft_val
+                    
+                soft_msk[msk==1] = 1
+                ### fixme softval-1_nonzero
+                #  soft_msk[msk!=0] = 1
+                msk=soft_msk
 
                 rl_feat = F.relu(self.conv(feature))
                 t = rl_feat*msk
@@ -669,7 +666,7 @@ class RegionProposalNetwork(torch.nn.Module):
 
         # 计算每个预测特征层上的预测目标概率和bboxes regression参数
         # objectness和pred_bbox_deltas都是list
-        if masks is not None: 
+        if isinstance(self.head, RPNMaskHead):# masks is not None: 
             objectness, pred_bbox_deltas = self.head(features, masks) # RPNMaskHead
         else:
             objectness, pred_bbox_deltas = self.head(features)
