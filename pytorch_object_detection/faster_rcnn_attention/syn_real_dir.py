cac22065d3c3d2ea7b37efa3be71f4fd03a6761b
yangxu351 <xuyang_ustb@163.com>
2022-09-15 09:48:14 -0400

    change paramenters and path

--- pytorch_object_detection/faster_rcnn_attention/syn_real_dir.py
+++ pytorch_object_detection/faster_rcnn_attention/syn_real_dir.py
@@ -3,9 +3,12 @@ import argparse
 from parameters import BASE_DIR
 
 
-def get_dir_arg(cmt='syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40', syn=True, workbase_data_dir='./real_syn_wdt_vockit'):
+def get_dir_arg(syn_cmt='', real_cmt='', workbase_data_dir='./real_syn_wdt_vockit', ):
+    ''' syn_cmt: syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40 
+        real_cmt: xilin_wdt
+    '''
     parser = argparse.ArgumentParser()
-    if syn:
+    if syn_cmt:
         parser.add_argument("--syn_base_dir", type=str, default=f'{BASE_DIR}/data/synthetic_data_wdt',
                             help="base path of synthetic data")
                             
@@ -26,6 +29,9 @@ def get_dir_arg(cmt='syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40', syn=True
 
         parser.add_argument("--syn_box_dir", type=str, default='{}/{}_gt_bbox/minr{}_linkr{}_px{}whr{}_all_annos_with_bbox',
                             help="syn box on image files \{syn_base_dir\}/{cmt}_gt_bbox/minr{}_linkr{}_px{}whr{}_all_annos_with_bbox")
+        parser.add_argument("--syn_workdir_data", type=str, default='{}/{}', help="workdir data base synwdt ./real_syn_wdt_vockit/\{cmt\}")
+        parser.add_argument("--syn_workdir_main", type=str, default='{}/Main', help="\{syn_workdir_data\}/Main ")    
+        
     else:
         parser.add_argument("--real_base_dir", type=str,default=f'{BASE_DIR}/data/wind_turbine', help="base path of synthetic data")
         parser.add_argument("--real_imgs_dir", type=str, default='{}/{}_crop', help="Path to folder containing real images")
@@ -33,9 +39,8 @@ def get_dir_arg(cmt='syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40', syn=True
         parser.add_argument("--real_yolo_annos_dir", type=str, default='{}/{}_crop_label_xcycwh', help="Path to folder containing real annos of yolo format")
         parser.add_argument("--real_voc_annos_dir", type=str, default='{}/{}_crop_label_xml_annos', help="Path to folder containing real annos of yolo format")
         parser.add_argument("--real_box_dir", type=str, default='{}/{}_crop_label_xml_bbox', help="Path to folder containing real annos of yolo format")
-        
-    parser.add_argument("--workdir_data", type=str, default='{}/{}', help="workdir data base synwdt ./real_syn_wdt_vockit/\{cmt\}")
-    parser.add_argument("--workdir_main", type=str, default='{}/Main', help="\{workdir_data\}/Main ")    
+        parser.add_argument("--real_workdir_data", type=str, default='{}/{}', help="workdir data base synwdt ./real_syn_wdt_vockit/\{cmt\}")
+        parser.add_argument("--real_workdir_main", type=str, default='{}/Main', help="\{real_workdir_data\}/Main ")    
                 
     parser.add_argument("--min_region", type=int, default=10, help="the smallest #pixels (area) to form an object")
     parser.add_argument("--link_r", type=int, default=10,
@@ -44,35 +49,42 @@ def get_dir_arg(cmt='syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40', syn=True
     parser.add_argument("--whr_thres", type=int, default=5,
                         help="ratio threshold of w/h or h/w")                        
     args = parser.parse_args()
-    if syn:
-        args.syn_data_dir = args.syn_data_dir.format(args.syn_base_dir, cmt)
-        args.syn_data_imgs_dir = args.syn_data_imgs_dir.format(args.syn_data_dir, cmt)
-        args.syn_data_segs_dir = args.syn_data_segs_dir.format(args.syn_data_dir, cmt)
+    if syn_cmt:
+        args.syn_data_dir = args.syn_data_dir.format(args.syn_base_dir, syn_cmt)
+        args.syn_data_imgs_dir = args.syn_data_imgs_dir.format(args.syn_data_dir, syn_cmt)
+        args.syn_data_segs_dir = args.syn_data_segs_dir.format(args.syn_data_dir, syn_cmt)
 
-        args.syn_yolo_annos_dir = args.syn_yolo_annos_dir.format(args.syn_base_dir, cmt, args.link_r, args.min_region, args.px_thres, args.whr_thres)
-        args.syn_voc_annos_dir = args.syn_voc_annos_dir.format(args.syn_base_dir, cmt, args.link_r, args.min_region, args.px_thres, args.whr_thres)
-        args.syn_box_dir = args.syn_box_dir.format(args.syn_base_dir, cmt, args.link_r, args.min_region, args.px_thres, args.whr_thres)
+        args.syn_yolo_annos_dir = args.syn_yolo_annos_dir.format(args.syn_base_dir, syn_cmt, args.link_r, args.min_region, args.px_thres, args.whr_thres)
+        args.syn_voc_annos_dir = args.syn_voc_annos_dir.format(args.syn_base_dir, syn_cmt, args.link_r, args.min_region, args.px_thres, args.whr_thres)
+        args.syn_box_dir = args.syn_box_dir.format(args.syn_base_dir, syn_cmt, args.link_r, args.min_region, args.px_thres, args.whr_thres)
 
-        args.workdir_data = args.workdir_data.format(workbase_data_dir, cmt)
+        args.syn_workdir_data = args.syn_workdir_data.format(workbase_data_dir, syn_cmt)
+        args.syn_workdir_main = args.syn_workdir_main.format(args.syn_workdir_data)
+        if not os.path.exists(args.syn_workdir_data):
+            os.makedirs(args.syn_workdir_data)
+        if not os.path.exists(args.syn_workdir_main):
+            os.makedirs(args.syn_workdir_main)
     else:
-        args.real_imgs_dir = args.real_imgs_dir.format(args.real_base_dir, cmt)
-        args.real_labelme_dir = args.real_labelme_dir.format(args.real_base_dir, cmt)
-        args.real_yolo_annos_dir = args.real_yolo_annos_dir.format(args.real_base_dir, cmt)
-        args.real_voc_annos_dir = args.real_voc_annos_dir.format(args.real_base_dir, cmt)
-        args.real_box_dir = args.real_box_dir.format(args.real_base_dir, cmt)
+        args.real_imgs_dir = args.real_imgs_dir.format(args.real_base_dir, real_cmt)
+        args.real_labelme_dir = args.real_labelme_dir.format(args.real_base_dir, real_cmt)
+        args.real_yolo_annos_dir = args.real_yolo_annos_dir.format(args.real_base_dir, real_cmt)
+        args.real_voc_annos_dir = args.real_voc_annos_dir.format(args.real_base_dir, real_cmt)
+        args.real_box_dir = args.real_box_dir.format(args.real_base_dir, real_cmt)
 
-        args.workdir_data = args.workdir_data.format(workbase_data_dir, cmt)
-    
+        args.real_workdir_data = args.real_workdir_data.format(workbase_data_dir, real_cmt)
+        args.real_workdir_main = args.real_workdir_main.format(args.real_workdir_data)
+        if not os.path.exists(args.real_workdir_data):
+            os.makedirs(args.real_workdir_data)
+        if not os.path.exists(args.real_workdir_main):
+            os.makedirs(args.real_workdir_main)
     # args.workdir_imgsets = args.workdir_imgsets.format(args.workdir_data)
-    args.workdir_main = args.workdir_main.format(args.workdir_data)
+    # args.real_workdir_data = args.real_workdir_data.format(args.real_workdir_data)
     # args.workdir_imgs = args.workdir_imgs.format(args.workdir_data)
     # args.workdir_annos = args.workdir_annos.format(args.workdir_data)
     # args.workdir_segs = args.workdir_segs.format(args.workdir_data)
 
     # if not os.path.exists(args.syn_voc_annos_dir):
     #     os.makedirs(args.syn_voc_annos_dir)
-    if not os.path.exists(args.workdir_data):
-        os.makedirs(args.workdir_data)
-    if not os.path.exists(args.workdir_main):
-        os.makedirs(args.workdir_main)        
+    
+    
     return args
